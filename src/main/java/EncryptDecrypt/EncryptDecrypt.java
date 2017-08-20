c210e7df63203ca5fc8983727a664978ec039d0a
Timothy Drake <tdrake@localhost.localdomain>
2017-08-20 14:07:26 +1200

    put tag on end of encrypted file to check see if this program encrypted it.

--- src/main/java/EncryptDecrypt/EncryptDecrypt.java
+++ src/main/java/EncryptDecrypt/EncryptDecrypt.java
@@ -21,6 +21,25 @@ public class EncryptDecrypt{
   private static final String ALGORITHM = "AES";
   private static final String TRANSFORM = "AES/CBC/PKCS5Padding";
   private static final byte[] IV = new byte[16];
+  private static final byte[] EN = "e".getBytes();
+
+  public static boolean isEncrypted(String oString){
+    try{
+      byte[] oStringBytes = Base64.getDecoder().decode(oString);
+      int lenCheck = oStringBytes.length-EN.length+1;
+      if(lenCheck < 0) return false;
+      int ENPlace = 0;
+      for(int i = lenCheck; i < oStringBytes.length;i++){
+        System.out.println("SBYTE: "+oStringBytes[i]+"\nEBYTE: "+EN[ENPlace]);
+        if(oStringBytes[i] != EN[ENPlace]){
+          return false;
+        }
+        ENPlace++;
+      }
+      return true;
+    }
+    catch(Exception e){return false;}
+  }
 
   protected static byte[] Apply(int cipherMode, byte[] original, String key)
   throws
@@ -53,7 +72,14 @@ public class EncryptDecrypt{
     IvParameterSpec ivspec = new IvParameterSpec(IV);
     cipher.init(Cipher.ENCRYPT_MODE,secretKey,ivspec);
     byte[] aName = cipher.doFinal(oName.getBytes());
-    String encodedaName = Base64.getEncoder().encodeToString(aName);
+    byte[] returnName = new byte[aName.length+EN.length];
+    for(int i = 0; i < aName.length;i++){
+      returnName[i] = aName[i];
+    }
+    for(int i = aName.length; i < EN.length;i++){
+      returnName[i] = EN[i];
+    }
+    String encodedaName = Base64.getEncoder().encodeToString(returnName);
     return encodedaName;
   }
 
@@ -71,7 +97,11 @@ public class EncryptDecrypt{
     IvParameterSpec ivspec = new IvParameterSpec(IV);
     cipher.init(Cipher.DECRYPT_MODE,secretKey,ivspec);
     byte[] decodedoName = Base64.getDecoder().decode(oName);
-    byte[] aName = cipher.doFinal(decodedoName);
+    byte[] removedtail = new byte[decodedoName.length-EN.length];
+    for(int i = 0; i < decodedoName.length-EN.length;i++){
+      removedtail[i] = decodedoName[i];
+    }
+    byte[] aName = cipher.doFinal(removedtail);
     return new String(aName);
   }
 
