ffa2582b26a58dab6b55639810134d9256e48275
Timothy Drake <tdrake@localhost.localdomain>
2017-08-23 12:30:25 +1200

    new libary for encrytpting

--- src/main/java/com/encryptdecrypt/EnDeCode.java
+++ src/main/java/com/encryptdecrypt/EnDeCode.java
@@ -1,4 +1,4 @@
-package com.endecode;
+package com.encryptdecrypt;
 
 import com.encryptdecrypt.*;
 import java.io.FileOutputStream;
@@ -12,57 +12,88 @@ import java .security.SecureRandom;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
+import java.security.Key;
 import org.apache.commons.io.IOUtils;
 
 public class EnDeCode{
   private static final String ALGORITHAM = "RSA";
   private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"), ".credentials/driveEncrypt");
-  private static final String PUBLICKEYNAME = "PublicKey";
-  private static final String PRIVATEKEYNAME = "PrivateKey";
-  private PrivateKey privateKey;
-  private PublicKey publicKey;
+  private static final String PUBLICKEYNAME = "PublicKey";//the name of the Public key.
+  private static final String PRIVATEKEYNAME = "PrivateKey";//the name of the private key.
+  protected static final int KEYSIZE = 128;
+  private static EnDeCode instance = null;//holds the instance for this.
+  private Key password; //holds the password being used for encryption
+  private PrivateKey privateKey;//holds the private key for signing.
+  private PublicKey publicKey;//holds the public key for others to check signiture.
 
-  public EnDeCode(String password, boolean genNew) throws Exception{
-    if(genNew){
-      generateKey(password);
+  protected static EnDeCode getInstance(String key) throws Exception{
+    if(instance == null){//gets a new instance of the class if null.
+      instance = new EnDeCode(key);//gets a new instance setting the key to be used.
     }
-    else{
-      FileInputStream fios = new FileInputStream(DATA_STORE_DIR.getAbsolutePath()+"/"+PRIVATEKEYNAME);
-      byte[] privatek = IOUtils.toByteArray(fios);
-      privatek = FileEncryptDecrypt.Decrypt(privatek, password);
-      fios.close();
-      fios = new FileInputStream(DATA_STORE_DIR.getAbsolutePath()+"/"+PUBLICKEYNAME);
-      byte[] publick = IOUtils.toByteArray(fios);
-      KeyFactory kf = KeyFactory.getInstance("RSA"); // or "EC" or whatever
-      privateKey = kf.generatePrivate(new PKCS8EncodedKeySpec(privatek));
-      publicKey = kf.generatePublic(new X509EncodedKeySpec(publick));
+    return instance;//returns the new instance.
+  }
+
+  protected Key getPassword(){
+    if(password == null){
+      System.out.println("Password is null");
+    }
+    return password;//returns the password used for encryption.
+  }
+  protected void setPassword(String key) throws Exception{
+    password = EncryptDecrypt.getKey(key);//sets the new password the generated key.
+  }
+
+  private EnDeCode(String key) throws Exception{
+    setPassword(key);
+  }
+
+  protected void loadKeys(String key) throws Exception{
+    try{
+      FileInputStream fios = new FileInputStream(DATA_STORE_DIR.getAbsolutePath()+"/"+PRIVATEKEYNAME);//trys to load in the private key from file.
+      byte[] privatek = IOUtils.toByteArray(fios);//gets the private key as a byte array.
+      FileEncryptDecrypt decrypt = FileEncryptDecrypt.getInstance(key);//gets a new instance to decrypt the private key.
+      EncryptDecrypt.checkSigniture = false;
+      privatek = decrypt.Decrypt(privatek);//decrypts the private key.
+      EncryptDecrypt.checkSigniture = true;
+      fios.close();//closes the file stream.
+      fios = new FileInputStream(DATA_STORE_DIR.getAbsolutePath()+"/"+PUBLICKEYNAME);//gets the public key.
+      byte[] publick = IOUtils.toByteArray(fios);//loads the public key into the byte array.
+      KeyFactory kf = KeyFactory.getInstance("RSA"); //gets the keyfactory instance to load public and private key.
+      privateKey = kf.generatePrivate(new PKCS8EncodedKeySpec(privatek));//sets the private key.
+      publicKey = kf.generatePublic(new X509EncodedKeySpec(publick));//sets the public key.
+    }
+    catch(Exception e){
+      e.printStackTrace();
+      generateKey(key);//generates a new public and private key.
     }
   }
 
 
-  private byte[] genSigniture(byte[] data){
-    Signature rsa = Signature.getInstance("SHA256withRSA");
-    rsa.initSign(privateKey);
-    rsa.update(data);
-    byte[] sig = rsa.sign();
+  protected byte[] genSigniture(byte[] data) throws Exception{
+    Signature rsa = Signature.getInstance("SHA256withRSA");//sets the sigiture instance to be used.
+    rsa.initSign(privateKey);//sets the key to be used for the signiture.
+    rsa.update(data);//loads in the file to generate the signiture.
+    byte[] sig = rsa.sign();//gets the byte array for the signiture.
+    return sig;//returns the new signiture.
   }
 
 
-  private void generateKey(String password) throws Exception{
-    KeyPairGenerator kpg = KeyPairGenerator.getInstance(ALGORITHAM);
-    SecureRandom rand = new SecureRandom();
-    kpg.initialize(1024,rand);
-    KeyPair keyPair = kpg.generateKeyPair();
-    privateKey = keyPair.getPrivate();
-    publicKey = keyPair.getPublic();
-    byte[] privatek = privateKey.getEncoded();
-    privatek = FileEncryptDecrypt.Encrypt(privatek,password);
-    byte[] publick = publicKey.getEncoded();
-    FileOutputStream fios = new FileOutputStream(DATA_STORE_DIR.getAbsolutePath()+"/"+PRIVATEKEYNAME);
-    fios.write(privatek);
-    fios.close();
-    fios = new FileOutputStream(DATA_STORE_DIR.getAbsolutePath()+"/"+PUBLICKEYNAME);
-    fios.write(publick);
-    fios.close();
+  private void generateKey(String key) throws Exception{
+    KeyPairGenerator kpg = KeyPairGenerator.getInstance(ALGORITHAM);//set the algoritan to use for the key pair.
+    SecureRandom rand = new SecureRandom();//gets a new secure random instance.
+    kpg.initialize(1024,rand);//sets the key size.
+    KeyPair keyPair = kpg.generateKeyPair();//generates a new key pair.
+    privateKey = keyPair.getPrivate();//saves the privae key.
+    publicKey = keyPair.getPublic();//saves the public key.
+    byte[] privatek = privateKey.getEncoded();//gets the private key as a byte array.
+    FileEncryptDecrypt encrypt = FileEncryptDecrypt.getInstance(key);//gets a new instance of the file encrypt.
+    privatek = encrypt.Encrypt(privatek);//encrypts the private key.
+    byte[] publick = publicKey.getEncoded();//gets the public key as a byte array.
+    FileOutputStream fios = new FileOutputStream(DATA_STORE_DIR.getAbsolutePath()+"/"+PRIVATEKEYNAME);//file stream to write out the pirvate key.
+    fios.write(privatek);//writes out the private key.
+    fios.close();//closes the file stream.
+    fios = new FileOutputStream(DATA_STORE_DIR.getAbsolutePath()+"/"+PUBLICKEYNAME);//file stream to wirte out the public key.
+    fios.write(publick);//writes out the public key.
+    fios.close();//closes the file stream.
   }
 }
